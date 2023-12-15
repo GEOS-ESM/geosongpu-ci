f39ad1f31756845c4d7a49ced7b2aaa40bb778c5
Florian Deconinck <deconinck.florian@gmail.com>
2023-12-15 14:10:08 -0500

    Clean up: Registry type hinting

--- geosongpu_ci/utils/registry.py
+++ geosongpu_ci/utils/registry.py
@@ -1,11 +1,11 @@
-from typing import Callable, ClassVar
+from typing import Dict, Type
 
 
 class Registry:
-    registry = {}
+    registry: Dict[str, Type] = {}
 
     @classmethod
-    def register(cls, to_register_cls):
+    def register(cls, to_register_cls: Type):
         if not isinstance(to_register_cls, type):
             raise RuntimeError("Register only registers function")
         cls.registry[to_register_cls.__name__] = to_register_cls
