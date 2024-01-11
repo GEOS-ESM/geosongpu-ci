4bc982c0160e163201e5a6bc12e20a5ca405155c
Florian Deconinck <deconinck.florian@gmail.com>
2024-01-11 16:04:51 -0500

    [sw] Refactor directories to split module & src Write 1.0.0 modulefiles

---
+++ sw_stack/discover/sles15/modulefiles/DSLwork/1.0.0.lua
@@ -0,0 +1,31 @@
+prereq("comp/gcc/12.3.0")
+prereq("nvidia/nvhpc-nompi/23.9")
+
+local install_dir  = "/discover/nobackup/projects/geosongpu/sw_sles15/v1.0.0/install/"
+
+-- UCX --
+local ucx_pkgdir = pathJoin(install_dir, "ucx")
+prepend_path("LD_LIBRARY_PATH",pathJoin(ucx_pkgdir,"lib"))
+
+-- OMPI --
+local ompi_pkgdir = pathJoin(install_dir, "ompi")
+
+setenv("M_MPI_ROOT",ompi_pkgdir)
+setenv("OPENMPI",ompi_pkgdir)
+setenv("MPI_HOME",ompi_pkgdir)
+
+prepend_path("PATH",pathJoin(ompi_pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(ompi_pkgdir,"lib"))
+prepend_path("INCLUDE",pathJoin(ompi_pkgdir,"include"))
+prepend_path("MANPATH",pathJoin(ompi_pkgdir,"share/man"))
+
+setenv("OMPI_MCA_orte_tmpdir_base","/tmp")
+setenv("TMPDIR","/tmp")
+setenv("OMP_STACKSIZE","1G")
+setenv("OMPI_MCA_mca_base_component_show_load_errors","0")
+setenv("PMIX_MCA_mca_base_component_show_load_errors","0")
+
+-- Python 3 --
+local py_pkgdir = pathJoin(install_dir, "python3")
+prepend_path("PATH",pathJoin(ompi_pkgdir,"bin"))
+prepend_path("LD_LIBRARY_PATH",pathJoin(ompi_pkgdir,"lib"))
