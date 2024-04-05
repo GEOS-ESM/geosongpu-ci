f1ab873b3f12f585f0238c5434944209ffc2da15
Florian Deconinck <deconinck.florian@gmail.com>
2024-04-05 15:48:49 -0400

    Rename for correct order

--- sw_stack/discover/sles15/src/2024.03.00/build_1_on-login.sh
+++ sw_stack/discover/sles15/src/2024.03.00/build_1_on-login.sh
@@ -5,7 +5,7 @@ source ./basics.sh
 
 echo " === Make NDSL venv === "
 cd $DSLSW_INSTALL_DIR
-./python3/bin/python3 -m venv venv
+python3 -m venv venv
 source ./venv/bin/activate
 pip install --upgrade setuptools pip
 pip install -e $DSLSW_INSTALL_DIR/ndsl
